e706157b5edee9d410d202236723e6b498075df2
Oyewale Oyediran <waleoyediran@gmail.com>
2015-09-08 20:22:27 +0100

    Initial Commit

---
+++ app/src/main/java/com/uyogist/uyogist/HomeActivity.java
@@ -0,0 +1,83 @@
+package com.uyogist.uyogist;
+
+import android.graphics.Color;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private NavigationView navigationView;
+    private DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
+        navigationView = (NavigationView) findViewById(R.id.drawer_view);
+        drawer = (DrawerLayout) findViewById(R.id.drawer);
+        setSupportActionBar(mToolbar);
+        setUpNavDrawer();
+    }
+
+    private void setUpNavDrawer() {
+        if (mToolbar != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            mToolbar.setNavigationIcon(R.drawable.ic_menu_white_24dp);
+            mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    drawer.openDrawer(GravityCompat.START);
+                }
+            });
+        }
+        navigationView.setNavigationItemSelectedListener(new NavigationListener());
+    }
+
+    private class NavigationListener implements NavigationView.OnNavigationItemSelectedListener{
+
+        @Override
+        public boolean onNavigationItemSelected(MenuItem menuItem) {
+            menuItem.setChecked(true);
+            switch (menuItem.getItemId()) {
+                case R.id.navigation_item_help:
+
+                    drawer.closeDrawers();
+                    return true;
+                default:
+                    return true;
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
