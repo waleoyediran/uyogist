5c52e3c7898ab0a1c3324395e26d512aace12880
Oyewale Oyediran <waleoyediran@gmail.com>
2015-09-10 02:32:48 +0100

    Added POST Gist API; connected with Datastore

--- appengine-server/main.py
+++ appengine-server/main.py
@@ -4,6 +4,7 @@
 from flask import Flask
 from flask import jsonify
 from flask.globals import request
+from models import GistModel
 
 app = Flask(__name__)
 # Note: We don't need to call run() since our application is embedded within
@@ -17,21 +18,29 @@ def hello():
 
 
 @app.route('/api/gist', methods=['GET', 'POST'])
-def gists():
+def api_gists():
     """Gists Endpoint"""
     if request.method == 'GET':
         data = {'data': 3}
         resp = jsonify(data)
         resp.status_code = 200
         return resp
-
     elif request.method == 'POST':
         data = request.get_json(force=True)
-        return jsonify(data)
+        gist = GistModel()
+        gist.added_by = data['nick']
+        gist.gist = data['gist']
+        key = gist.put()
+
+        resp = jsonify(dict(gist.to_dict(), **{'id': key.urlsafe()}))
+        resp.status_code = 201
+        return resp
+    else:
+        raise RuntimeError("Unimplemented HTTP method")
 
 
 @app.route('/api/gist/<gist_id>', methods=['GET'])
-def api_users(gist_id):
+def api_gist(gist_id):
     gists = {'1': 'gist 1', '2': 'gist 2', '3': 'gist 3'}
 
     if gist_id in gists:
@@ -64,3 +73,4 @@ def application_error(e):
     resp.status_code = 500
 
     return resp
+
